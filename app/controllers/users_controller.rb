d8cbda3a6c39b06af3f45a50c50e8f12f8af1a18
drewviscomi <drew.viscomi@kellogg.northwestern.edu>
2026-03-10 22:03:15 +0000

    hw4 update 3/10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @user["username"] = params["username"]
-    @user["email"] = params["email"]
-    @user["password"] = BCrypt::Password.create(params["password"])
-    @user.save
-    redirect_to "/"
+    @existing_user = User.find_by({ "email" => params["email"] })
+    if @existing_user != nil
+      flash["notice"] = "Email already taken."
+      redirect_to "/users/new"
+    else
+      @user = User.new
+      @user["username"] = params["username"]
+      @user["email"] = params["email"]
+      @user["password"] = BCrypt::Password.create(params["password"])
+      @user.save
+      redirect_to "/"
+    end
   end
 end
