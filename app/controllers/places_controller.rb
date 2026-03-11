a5340fb4c10f8dd8762f8f806ac81512bc73c0d5
drewviscomi <drew.viscomi@kellogg.northwestern.edu>
2026-03-11 18:27:07 +0000

    hw4 scope places to users 3/11

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,39 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    if @current_user != nil
+      @places = Place.where({ "user_id" => @current_user["id"] })
+    else
+      flash["notice"] = "Login first."
+      redirect_to "/login"
+    end
   end
 
   def show
-    @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"], "user_id" => session["user_id"] })
+    if @current_user != nil
+      @place = Place.find_by({ "id" => params["id"] })
+      @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @current_user["id"] })
+    else
+      flash["notice"] = "Login first."
+      redirect_to "/login"
+    end
   end
 
   def new
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["name"]
-    @place.save
-    redirect_to "/places"
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @place = Place.new
+      @place["name"] = params["name"]
+      @place["user_id"] = @user["id"]
+      @place.save
+      redirect_to "/places"
+    else
+      flash["notice"] = "Login first."
+      redirect_to "/login"
+    end
   end
 
 end
